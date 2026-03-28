7eaf474f49f16521262450ec05594e8f38579a78
Andrew Malyukov <ds-an@tuta.io>
2026-03-28 19:11:52 +0300

    feat: add check formatting job for gh CI

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -10,33 +10,33 @@ import (
 
 func TestAuth(t *testing.T) {
 	tests := []struct {
-		name string
-		input http.Header 
-		want string
-    wantErr bool
+		name    string
+		input   http.Header
+		want    string
+		wantErr bool
 	}{
 		{
 			name: "Happy",
 			input: http.Header{
 				"Authorization": []string{"ApiKey sdlfksfansldfj"},
 			},
-			want: "sdlfksfansldfj",
+			want:    "sdlfksfansldfj",
 			wantErr: false,
 		},
 		{
-			name: "Empty header",
-			input: http.Header{},
-			want: "",
+			name:    "Empty header",
+			input:   http.Header{},
+			want:    "",
 			wantErr: true,
 		},
 	}
-    for i, tc := range tests {
-        got, gotErr := auth.GetAPIKey(tc.input)
-        if (gotErr != nil) != tc.wantErr {
-            t.Fatalf("test %d, \"%s\": expected error: %v, got: %v", i+1, tc.name, tc.wantErr, gotErr)
-        }
-        if !reflect.DeepEqual(tc.want, got) {
-            t.Fatalf("test %d, \"%s\": expected: %v, got: %v", i+1, tc.name, tc.want, got)
-        }
-    }
+	for i, tc := range tests {
+		got, gotErr := auth.GetAPIKey(tc.input)
+		if (gotErr != nil) != tc.wantErr {
+			t.Fatalf("test %d, \"%s\": expected error: %v, got: %v", i+1, tc.name, tc.wantErr, gotErr)
+		}
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("test %d, \"%s\": expected: %v, got: %v", i+1, tc.name, tc.want, got)
+		}
+	}
 }
